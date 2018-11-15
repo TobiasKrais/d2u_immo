7cb674828b423aff88442b7fdc7aa89f39c1d774
Tobias Krais <tk@design-to-use.de>
2018-11-15 16:19:32 +0100

    prepare next version

--- pages/setup.php
+++ pages/setup.php
@@ -31,6 +31,10 @@ $d2u_module_manager->showManagerList();
 <p>Sammelthread fürs Addon im <a href="https://redaxo.org/forum/viewtopic.php?f=43&t=21965" target="_blank">Redaxo Forum</a>.</p>
 <p>Fehlermeldungen bitte im <a href="https://github.com/TobiasKrais/d2u_immo" target="_blank">GitHub Repository</a> melden.</p>
 <h2>Changelog</h2>
+<p>1.0.8:</p>
+<ul>
+	<li>...</li>
+</ul>
 <p>1.0.7:</p>
 <ul>
 	<li>Bugfix: Deaktiviertes Addon zu deinstallieren führte zu fatal error.</li>
