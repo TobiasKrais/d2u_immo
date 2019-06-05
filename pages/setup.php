187ff2dba64bc1fdc3586056839f2d4d6afdcebd
Tobias Krais <tk@design-to-use.de>
2019-06-05 21:46:22 +0200

    prepare next version

--- pages/setup.php
+++ pages/setup.php
@@ -31,6 +31,10 @@ $d2u_module_manager->showManagerList();
 <p>Fehlermeldungen bitte im <a href="https://github.com/TobiasKrais/d2u_immo" target="_blank">GitHub Repository</a> melden.</p>
 <h2>Changelog</h2>
 
+<p>1.1.4-DEV:</p>
+<ul>
+	<li>...</li>
+</ul>
 <p>1.1.3:</p>
 <ul>
 	<li>Bugfix: Fatal error beim Speichern verursacht durch die URL Addon Version 2 Anpassungen behoben.</li>
