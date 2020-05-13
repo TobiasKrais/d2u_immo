d2d2093b3b7e044b0dd28746d27c6332a26d7b22
Tobias Krais <tk@design-to-use.de>
2020-05-13 15:24:53 +0200

    fix link & no default contact

--- pages/setup.php
+++ pages/setup.php
@@ -36,6 +36,7 @@ $d2u_module_manager->showManagerList();
 	<li>Fehler behoben, wenn bei einer Immobilie in der Übersichtsliste auf offline geklickt wurde.</li>
 	<li>Modul 70-1 "D2U Immo Addon - Hauptausgabe" leitet Offlineobjekte auf die Fehlerseite weiter.</li>
 	<li>Modul 70-2 "D2U Immo Addon - Infobox Ansprechpartner" hat bei Klick auf E-Mail nicht mehr das Angebotstab angezeigt.</li>
+	<li>Modul 70-2 "D2U Immo Addon - Infobox Ansprechpartner" hat kann jetzt auf Fallbackkontakt verzichten.</li>
 	<li>Aktualisiert beim Speichern automatisch den search_it index.</li>
 	<li>Fehler entfernt wenn in den Einstellungen kein Artikel gesetzt ist.</li>
 	<li>Notice beim Speichern der Einstellungen entfernt.</li>
