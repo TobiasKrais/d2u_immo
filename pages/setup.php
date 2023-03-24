0766b6b9efef9b940f38d23110b89f20e72863c4
Tobias Krais <tk@design-to-use.de>
2023-03-24 13:47:17 +0100

    prepare 1.1.8

--- pages/setup.php
+++ pages/setup.php
@@ -31,6 +31,10 @@ $d2u_module_manager->showManagerList();
 <p>Fehlermeldungen bitte im <a href="https://github.com/TobiasKrais/d2u_immo" target="_blank">GitHub Repository</a> melden.</p>
 <h2>Changelog</h2>
 
+<p>1.1.8-DEV:</p>
+<ul>
+	<li>...</li>
+</ul>
 <p>1.1.7:</p>
 <ul>
 	<li>PHP-CS-Fixer Code Verbesserungen.</li>
