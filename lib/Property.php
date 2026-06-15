ab9c12069cdf3efcacb5b9e9c3f3d3761a92d751
Tobias Krais <tk@design-to-use.de>
2026-06-15 12:12:10 +0200

    fix potential fatal error

--- lib/Property.php
+++ lib/Property.php
@@ -967,19 +967,23 @@ class Property implements \TobiasKrais\D2UHelper\ITranslationHelper
                         .'description_equipment = :description_equipment, '
                         .'description_location = :description_location, '
                         .'description_others = :description_others, '
-                        ."documents = '". implode(',', $this->documents) ."', "
-                        ."teaser = '". $this->teaser ."', "
+                        .'documents = :documents, '
+                        .'teaser = :teaser, '
                         .'name = :name, '
-                        ."translation_needs_update = '". $this->translation_needs_update ."', "
+                        .'translation_needs_update = :translation_needs_update, '
                         .'updatedate = CURRENT_TIMESTAMP, '
-                        ."updateuser = '". (rex::getUser() instanceof rex_user ? rex::getUser()->getLogin() : '') ."' ";
+                        .'updateuser = :updateuser ';
                 $result = rex_sql::factory();
                 $result->setQuery($query, [
                     ':description' => htmlspecialchars($this->description),
                     ':description_equipment' => htmlspecialchars($this->description_equipment),
                     ':description_location' => htmlspecialchars($this->description_location),
                     ':description_others' => htmlspecialchars($this->description_others),
+                    ':documents' => implode(',', $this->documents),
+                    ':teaser' => $this->teaser,
                     ':name' => $this->name,
+                    ':translation_needs_update' => $this->translation_needs_update,
+                    ':updateuser' => rex::getUser() instanceof rex_user ? rex::getUser()->getLogin() : '',
                 ]);
                 $error = $result->hasError();
 
